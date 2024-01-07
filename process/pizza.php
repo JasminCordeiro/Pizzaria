eaefe2d420daf2d897b64cea84c07180b1392960
Jasmin Cordeiro <playgame0159@gmail.com>
2024-01-07 20:59:49 -0300

    Initial commit

---
+++ process/pizza.php
@@ -0,0 +1,78 @@
+<?php 
+
+    include_once("conn.php");
+
+
+
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    if ($method === "GET") {
+
+        $bordasQuery = $conn->query("SELECT * FROM bordas;");
+
+        $bordas = $bordasQuery->fetchAll();
+
+        
+        $massasQuery = $conn->query("SELECT * FROM massas;");
+
+        $massas = $massasQuery->fetchAll();
+
+        
+        $saboresQuery = $conn->query("SELECT * FROM sabores;");
+
+        $sabores = $saboresQuery->fetchAll();
+         
+        
+    } else if($method === "POST"){
+        
+        $data = $_POST;
+        
+        $borda = $data["borda"];
+        $massa = $data["massa"];
+        $sabores = $data["sabores"];
+
+        if (count($sabores)>3) {
+
+            $_SESSION["msg"] = "Selecione no máximo 3 sabores!";
+            $_SESSION["status"] = "warning";
+
+            
+        } else {
+            
+            $stmt = $conn->prepare("INSERT INTO pizzas (borda_id, massa_id) VALUES (:borda,:massa)");
+            $stmt->bindParam(":borda", $borda, PDO::PARAM_INT);
+            $stmt->bindParam(":massa", $massa, PDO::PARAM_INT);
+
+            $stmt->execute();
+
+            $pizzaId = $conn->lastInsertId();
+
+            $stmt = $conn->prepare("INSERT INTO pizza_sabor (pizza_id, sabor_id) VALUES (:pizza,:sabor)");
+
+            foreach($sabores as $sabor){
+                $stmt->bindParam(":pizza", $pizzaId, PDO::PARAM_INT);
+                $stmt->bindParam(":sabor", $sabor, PDO::PARAM_INT);
+
+                $stmt->execute();
+
+            }
+
+            $stmt = $conn->prepare("INSERT INTO pedidos (pizza_id, status_id) VALUES (:pizza,:status)");
+
+            $statusId = 1;
+            $stmt->bindParam(":pizza", $pizzaId);
+            $stmt->bindParam(":status", $statusId);
+
+            $stmt->execute();
+
+
+            $_SESSION["msg"] = "Pedido realizado com sucesso!";
+            $_SESSION["status"] = "success";
+
+        }
+        
+        header("Location:..");
+
+    }
+    
+?>
---
+++ templates/header.php
@@ -0,0 +1,56 @@
+<?php 
+
+    include("process/conn.php");
+
+    $msg = "";
+
+if(isset($_SESSION["msg"])){
+
+    $msg = $_SESSION["msg"];
+    $status = $_SESSION["status"];
+
+    $_SESSION["msg"] = "";
+    $_SESSION["status"] = "";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça seu pedido!</title>
+    <!--Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- App CSS --> 
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+        <a href="index.php" class="navbar-brand">
+            <img src="img/pizza.svg" alt="Pizzaria" id="brand-logo">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a href="index.php" class="nav-link">Peça sua pizza</a>
+                </li>
+            </ul>
+        </div>
+        </nav>
+    </header>
+    <?php if($msg !=""):?>
+        <div class="alert alert-<?= $status ?>">
+            <p><?= $msg ?></p>
+        </div>
+    <?php endif; ?>
+
+
+
